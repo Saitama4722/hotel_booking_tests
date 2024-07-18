04024c170a69129ceaf45d285095cdb3a3532cc7
Saitama4722 <daitama@yandex.ru>
2024-07-18 21:37:51 +0300

    Set up CI/CD and QASE integration

--- upload_qase_results.py
+++ upload_qase_results.py
@@ -1,12 +1,15 @@
 import json
 from qaseio import QaseApi
 
+# Загрузка конфигурации QASE из файла
 with open('qase_config.json') as f:
     config = json.load(f)
 
+# Инициализация API QASE
 api = QaseApi(config['apiToken'])
 project_code = config['projectCode']
 results_file = "results.xml"
 
+# Загрузка результатов тестирования в QASE
 response = api.results.create_from_junit(project_code, results_file)
 print(response)  # Для проверки ответа от API
