3edeafcdf6bf82480a8f2e43665d7b754344a8d2
Saitama4722 <daitama@yandex.ru>
2024-07-18 18:54:38 +0300

    Add CI/CD configuration and update tests

--- tests/test_create_booking.py
+++ tests/test_create_booking.py
@@ -23,7 +23,24 @@ def test_create_booking_failure():
     data = {
         "firstname": "Jim",
         "lastname": "Brown",
-        "totalprice": "eleven",
+        "totalprice": 111,
+        "depositpaid": "not-boolean",  # Некорректное значение для поля depositpaid
+        "bookingdates": {
+            "checkin": "2024-07-14",
+            "checkout": "2024-07-15"
+        },
+        "additionalneeds": "Breakfast"
+    }
+    response = requests.post(url, json=data)
+    print(response.status_code)  # Для отладки
+    print(response.text)         # Для отладки
+    assert response.status_code == 400  # Ожидаем код состояния 400
+
+def test_create_booking_failure_missing_field():
+    url = "https://restful-booker.herokuapp.com/booking"
+    data = {
+        "lastname": "Brown",  # Отсутствует firstname
+        "totalprice": 111,
         "depositpaid": True,
         "bookingdates": {
             "checkin": "2024-07-14",
@@ -32,4 +49,6 @@ def test_create_booking_failure():
         "additionalneeds": "Breakfast"
     }
     response = requests.post(url, json=data)
-    assert response.status_code == 500
+    print(response.status_code)  # Для отладки
+    print(response.text)         # Для отладки
+    assert response.status_code == 400  # Ожидаем код состояния 400
