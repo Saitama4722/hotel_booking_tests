c028f9bb876edf902d796cc5985f7e71f9cba6cc
Saitama4722 <daitama@yandex.ru>
2024-07-18 21:04:16 +0300

    Update test results upload

--- upload_qase_results.py
+++ upload_qase_results.py
@@ -1,18 +1,12 @@
 import json
 from qaseio import QaseApi
 
-# Загрузка конфигурации из файла qase_config.json
 with open('qase_config.json') as f:
     config = json.load(f)
 
-# Инициализация API клиента с использованием API токена
 api = QaseApi(config['apiToken'])
-
-# Получение кода проекта из конфигурации
 project_code = config['projectCode']
-
-# Указание пути к файлу с результатами тестов
 results_file = "results.xml"
 
-# Загрузка результатов тестов в QASE
-api.results.create_from_junit(project_code, results_file)
+response = api.results.create_from_junit(project_code, results_file)
+print(response)  # Добавьте это для проверки ответа от API
